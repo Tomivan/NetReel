5b9bd8afa8dacff4ce31f032b0962a552785dffc
Terra-bunkunmi <bukunmiodugbesan@Terra-016.local>
2019-12-14 09:31:38 +0100

    Project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginComponent } from './login/login.component';
+import { OptionsComponent } from './options/options.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'landing-page', component: LandingPageComponent },
+  {path: 'login', component: LoginComponent},
+  {path: 'options', component: OptionsComponent},
+  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo: 'landing-page', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
